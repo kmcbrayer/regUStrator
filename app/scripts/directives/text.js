d501e1f481d434c97582d5826dff643477a27ea2
kmcbrayer <kmcbrayer77@gmail.com>
2014-08-05 21:05:12 -0400

    inner text complete. next use text geometry efficiently

--- app/scripts/directives/text.js
+++ app/scripts/directives/text.js
@@ -3,15 +3,18 @@
 angular.module('regUstratorApp')
   .directive('text', function (sceneData) {
     return {
-      template: '<div></div>',
+      template: '<div ng-transclude></div>',
       restrict: 'E',
+      transclude: true,
+      replace: true,
       link: function postLink(scope, element, attrs) {
         // create a canvas element
+        var innerText = element.html().split(/[><]/)[2];//so bad
 				var canvas1 = document.createElement('canvas');
 				var context1 = canvas1.getContext('2d');
 				context1.font = "Bold 10px Arial";
 				context1.fillStyle = "rgba(255,0,0,0.95)";
-				context1.fillText('Hello, world!', 0,  10);
+				context1.fillText(innerText, 0, 10);
 
 				// canvas contents will be used for a texture
 				var texture1 = new THREE.Texture(canvas1); 
@@ -21,7 +24,7 @@ angular.module('regUstratorApp')
 				material1.transparent = true;
 
 				var mesh1 = new THREE.Mesh(
-				  new THREE.PlaneGeometry(2, 2),
+				  new THREE.PlaneGeometry(5, 5),
 				  material1
 				);
 				mesh1.position.set(0,0,0);
